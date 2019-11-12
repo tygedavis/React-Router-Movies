c250601242b8874fe3ae1052de3748cd36f5ddbb
tygedavis <tyge.davis@gmail.com>
2019-11-12 16:34:15 -0700

    Home button now has functionality, links to home page

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const SavedList = props => (
+function SavedList(props){
+
+  return(
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <Link to='/' className="home-button">Home</Link>
   </div>
-);
+  )};
 
 export default SavedList;
